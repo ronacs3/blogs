6bb2dc28d9be278fe62b0d09c9612e83a74e153d
ronacs3 <quanghoangminh2001@gmail.com>
2023-03-07 11:25:57 +0700

    update

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,6 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
 import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
@@ -19,49 +18,8 @@ import MoreIcon from '@mui/icons-material/MoreVert';
 import { Info } from "@mui/icons-material";
 import { Stack } from "@mui/material";
 import { createTheme } from '@mui/material/styles';
-import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
-import { InstantSearch, SearchBox, Hits, Highlight, Configure } from "react-instantsearch-dom";
 import { useState } from "react";
 import WhatshotIcon from '@mui/icons-material/Whatshot';
-// const Search = styled('div')(({ theme }) => ({
-//   position: 'relative',
-//   borderRadius: theme.shape.borderRadius,
-//   backgroundColor: alpha(theme.palette.common.black, 0.15),
-//   '&:hover': {
-//     backgroundColor: alpha(theme.palette.common.black, 0.25),
-//   },
-//   marginRight: theme.spacing(2),
-//   marginLeft: 0,
-//   width: '100%',
-//   [theme.breakpoints.up('sm')]: {
-//     marginLeft: theme.spacing(3),
-//     width: 'auto',
-//   },
-// }));
-
-// const SearchIconWrapper = styled('div')(({ theme }) => ({
-//   padding: theme.spacing(0, 2),
-//   height: '100%',
-//   position: 'absolute',
-//   pointerEvents: 'none',
-//   display: 'flex',
-//   alignItems: 'center',
-//   justifyContent: 'center',
-// }));
-
-// const StyledInputBase = styled(InputBase)(({ theme }) => ({
-//   color: 'inherit',
-//   '& .MuiInputBase-input': {
-//     padding: theme.spacing(1, 1, 1, 0),
-//     // vertical padding + font size from searchIcon
-//     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-//     transition: theme.transitions.create('width'),
-//     width: '100%',
-//     [theme.breakpoints.up('md')]: {
-//       width: '20ch',
-//     },
-//   },
-// }));
 
 export default function Header({}) {
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
